af1f7fcdaa75e9707822f9ff1bd959cdc2160fd8
abingzcr <750321038@qq.com>
2022-09-05 08:51:33 +0800

    Site updated: 2022-09-05 08:51:32

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -34,7 +34,8 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
+			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
+			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/"
 		});
 	});
 }
